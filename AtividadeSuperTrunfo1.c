2c95b6f025e6a55d9d9e39ae2fe7a2d0fb891939
BIANCA SOARES DE OLIVEIRA <biancasoares_oliveira@hotmail.com>
2026-02-25 00:01:09 +0000

    inicioatividade

---
+++ AtividadeSuperTrunfo1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main (){
+    char estadoA = "A";
+    char codigo[3] ;
+    char nomeCidade[30];
+    int populacao;
+    float areaKm;
+    float pib;
+    int pontosTuristicos; 
+
+    
+
+
+
+
+
+
+
+
+
+
+}
+
