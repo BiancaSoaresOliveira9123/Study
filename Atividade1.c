86bd0dd1f3b5fe9550cecdb0610265d80e9cb66f
Bianca Soares <biancasoares_oliveira@hotmail.com>
2026-05-19 12:46:13 -0300

    atividade1 printf e scanf

---
+++ Atividade1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    int idade;
+    float altura;
+    char nome[50];
+    int matricula;
+
+    printf("Digite a sua idade:\n");
+    scanf("%d", &idade);
+    printf("Digite a sua altura:\n");
+    scanf("%f", &altura);
+    printf("Digite o seu nome:\n");
+    scanf("%s", nome);
+    printf("Digite a sua matrícula: \n");
+    scanf("%d", &matricula);
+
+    printf("Nome do aluno %s -  Matrícula: %d\n", nome, matricula);
+    printf("Idade: %d  - Altura: %.2f\n", idade, altura);
+
+
+    return 0;
+
+
+}
